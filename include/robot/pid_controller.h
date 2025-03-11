85c6c2a89535656c1d54d464ba5b5579a843c7ac
MuditUpadhyay04 <mudit.upadhyay04@gmail.com>
2025-03-10 22:07:00 -0500

    removed dt

--- include/robot/pid_controller.h
+++ include/robot/pid_controller.h
@@ -3,12 +3,11 @@
 
 class PIDcontroller {
 public:
-    //PIDcontroller(double kp, double ki, double kd, double dt, double min, double max);
-    PIDcontroller(double kp, double ki, double kd, double dt, double min, double max){
+    //PIDcontroller(double kp, double ki, double kd, double min, double max);
+    PIDcontroller(double kp, double ki, double kd, double min, double max){
         this->kp = kp;
         this->ki = ki;
         this->kd = kd;
-        this->dt = dt;
         this->minOutput = min;
         this->maxOutput = max;
         this->prev_error = 0;
@@ -17,8 +16,7 @@ public:
         double  Compute(double setpoint, double actual_value, double dt);
         void    Reset(); //Extra space between void and reset because I can't see uneven indent and spacing after doing python for a long time
 
-    double kp, ki, kd;      // PID gains
-    double dt;              // Time step
+    double kp, ki, kd;      // PID gains     
     double minOutput, maxOutput;  // Output limits
 
     double prev_error;      // Previous error
