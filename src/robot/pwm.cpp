87a33c4a76978226152256320cd6a425925b0ddd
xXDMOGXx <83487906+xXDMOGXx@users.noreply.github.com>
2025-03-12 03:50:27 -0500

    Undo the LEDC changes and just use analogWriteResolution

--- src/robot/pwm.cpp
+++ src/robot/pwm.cpp
@@ -6,36 +6,29 @@
 
 // Built-In Libraries
 #include "Arduino.h"
-#include "driver/ledc.h"
 
 // Custom Libraries
 #include "utils/logging.h"
 #include "utils/config.h"
 #include "utils/functions.h"
 
-constexpr static uint8_t LEDC_DUTY_RES = 13;
-static uint16_t LEDC_DUTY_STEPS = (1 << LEDC_DUTY_RES) - 1;
-constexpr static int16_t LEDC_FREQUENCY = 5000;
-
 // Sets a pin to be able to output
-void setupPWM(int pin, int channel) {
+void setupPWM(int pin) {
     pinMode(pin, OUTPUT);
-    ledcSetup(channel, LEDC_FREQUENCY, LEDC_DUTY_RES);
-    ledcAttachPin(pin, channel);
 }
 
 // Writes PWM to a pin. The duty cycle is a number
 // between 0 and 255 that determines how often the signal is HIGH.
 // You can refer to this link for more information on PWM
 // https://learn.sparkfun.com/tutorials/pulse-width-modulation/all
-void writePWM(int channel, int dutyCycle) {
-    ledcWrite(channel, dutyCycle);
+void writePWM(int pin, int dutyCycle) {
+    analogWrite(pin, dutyCycle);
 }
 
 // Takes in a power between [0, 1]
-// We use this to change a float power between 0-1 to an int duty cycle between 0-2^LEDC_DUTY_RES
+// We use this to change a float power between 0-1 to an int duty cycle between 0-255
 int mapPowerToDuty(float power) {
-    int value = fmap(power, 0.0, 1.0, 0.0, LEDC_DUTY_STEPS);
+    int value = fmap(power, 0.0, 1.0, 0.0, 255);
     serialLog((char*)"Mapped Duty: ", 4);
     serialLogln(value, 4);
     return value;
