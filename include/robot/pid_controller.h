ecc67c257b8ed3cccf6967b481ba7b47c6b65b98
MuditUpadhyay04 <mudit.upadhyay04@gmail.com>
2025-03-29 02:01:20 -0500

    Removed duplication

--- include/robot/pid_controller.h
+++ include/robot/pid_controller.h
@@ -7,8 +7,7 @@ public:
     // PIDController(double kp, double ki, double kd, double min, double max);
     PIDController(double kp, double ki, double kd, double min, double max) : kp(kp), ki(ki), kd(kd), minOutput(min), maxOutput(max), prev_error(0), integral(0) {}
 
-    double ComputePosition(double setpoint, double actual_value, double dt);
-    double ComputeVelocity(double target, double actual_vel_value, double dt);
+    double Compute(double setpoint, double actual_value, double dt);
     void Reset(); // Extra space between void and reset because I can't see uneven indent and spacing after doing python for a long time
 
     double kp, ki, kd;           // PID gains
--- src/robot/pid_controller.cpp
+++ src/robot/pid_controller.cpp
@@ -21,7 +21,7 @@
 
 using namespace std;
 
-double PIDController::ComputePosition(double setpoint, double actual_value, double dt) {
+double PIDController::Compute(double setpoint, double actual_value, double dt) {
     // Calculate error
     double error = setpoint - actual_value;
 
@@ -51,42 +51,7 @@ double PIDController::ComputePosition(double setpoint, double actual_value, doub
     }
 
     // Clamp output
-    if (output < minOutput) {
-        output = minOutput;
-    } else if (output > maxOutput) {
-        output = maxOutput;
-    }
-
-    return output;
-}
-
-double PIDController::ComputeVelocity(double target, double actual_vel_value, double dt) {
-    // Calculate error
-    double error = target - actual_vel_value;
-
-    // Proportional term
-    double val_p = kp * error; //We will be messing with this while calibrating
-
-    // Integral term
-    integral += error * dt;
-    double val_i = ki * integral;
- 
-    // Derivative term
-    double derivative = (error - prev_error) / dt;
-    double val_d = kd * derivative;
-
-    // Calculate total output
-    double output = val_p + val_i + val_d;
-
-    // Save error to previous error
-    prev_velocity_error = error;
-
-    if ((prev_error > 0 && error < 0) || (prev_error < 0 && error > 0)) {
-        Reset();
-    }
-
-    // Clamp output
-    output = std::max(minOutput, std::min(maxOutput, output));
+    output = max(minOutput, min(maxOutput, output));
 
     return output;
 }
@@ -94,7 +59,3 @@ double PIDController::ComputeVelocity(double target, double actual_vel_value, do
 void PIDController::Reset(){
     integral = 0;
 }
-
-// double PIDController::ActualController() {
-    
-// }
